5b6417300c1d4af7e4e92d86bb33a67e2ea0b6b2
Lauren Ventura <you@example.com>
2018-03-17 14:46:51 -0400

    added initial pseudocoding - Lauren

--- assets/js/modules/spotify.js
+++ assets/js/modules/spotify.js
@@ -1,1 +1,38 @@
-//SPOTIFY API 
+//SPOTIFY API
+
+//create function to convert score to mood
+// var moodMaker = function(score){
+//     if (score <= 20) {
+//         return score = "mad";
+//     }
+//     if (score <= 40 && score > 20){
+//         return score = "sad";
+//     }
+//     if (score <= 60 && score > 40){
+//         return score = "neutral";
+//     }
+//     if (score <= 80 && score > 60){
+//         return score = "happy";
+//     }
+//     if (score <= 100 && score > 80){
+//         return score = "ecstatic";
+//     }
+// }
+
+//azureScore = moodmaker(azure score);
+
+//get songIds from the database
+    //store them into an array?
+
+//var getSongFeatures = function () {
+    //var displaySongs = [];
+    //create for loop that runs for the length of the songIds in the database
+        //create the ajax call that queries https://api.spotify.com/v1/tracks/{id[i]}
+        //get response.energy and convert it to more usable number Math.round(response.energy * 100);
+        //var energyScore = moodMaker(response.energy);
+        //compare energyScore to azureScore
+            // if (energyScore === azureScore){
+                //push to displaySongs[];
+            // }
+    //return displaySongs;
+//}
