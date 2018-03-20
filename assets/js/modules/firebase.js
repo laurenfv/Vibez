f31f6d869018c1582007953e91da88dc59b3c7a4
VirtualVasquez <mxv647@med.miami.edu>
2018-03-20 18:20:55 -0400

    Good for testing

--- assets/js/modules/firebase.js
+++ assets/js/modules/firebase.js
@@ -40,36 +40,37 @@ console.log("okay");
 
 //Need to read/pull array of the songs from Firebase
 var songIDs = SongsRef.on('value', function(snapshot){
-	console.log(snapshot.val().song00);
-	console.log(snapshot.val().song01);
-	console.log(snapshot.val().song02);
-	console.log(snapshot.val().song03);
-	console.log(snapshot.val().song04);
-	//need to loop from song00 to song99, how?
-	console.log(snapshot.val().song05);
-	console.log(snapshot.val().song06);
-	console.log(snapshot.val().song07);
-	console.log(snapshot.val().song08);
-	console.log(snapshot.val().song09);
-	console.log(snapshot.val().song10);
-	console.log(snapshot.val().song11);
+	console.log(Object.values(snapshot.val()));
+	// console.log(snapshot.val().song01);
+	// console.log(snapshot.val().song02);
+	// console.log(snapshot.val().song03);
+	// console.log(snapshot.val().song04);
+	// //need to loop from song00 to song99, how?
+	// console.log(snapshot.val().song05);
+	// console.log(snapshot.val().song06);
+	// console.log(snapshot.val().song07);
+	// console.log(snapshot.val().song08);
+	// console.log(snapshot.val().song09);
+	// console.log(snapshot.val().song10);
+	// console.log(snapshot.val().song11);
 
 });
 //Azure API Key
 ///////////////////////////////////////////////////////
 //STILL NEED CLARIFICATION ON HOW TO USE THE FUNCTIONS
 ///////////////////////////////////////////////////////
-var AzureKey1 = AzureRef.on('value', function(snapshot){
-	console.log(snapshot.val().Key1);
+var AzureKey1 = AzureRef.once('value', function(snapshot){
+	window.azureKey1 = snapshot.val().Key1;
+	console.log(window.azureKey1);
 });
 ////IF we need it, here is the 2nd key
-var AzureKey2 = AzureRef.on('value', function(snapshot){
-	console.log(snapshot.val().Key2);
-});
-////////////////////////////////////////////////////////
+// var AzureKey2 = AzureRef.on('value', function(snapshot){
+// 	console.log(snapshot.val().Key2);
+// });
+// ////////////////////////////////////////////////////////
 
-//Spotify - API Key not yet defined, but this calls it
-////////////////////////////////////////////////////////
-var SpotifyKey = SpotifyRef.on('value', function(snapshot){
-	console.log(snapshot.val().Key);
-});
+// //Spotify - API Key not yet defined, but this calls it
+// ////////////////////////////////////////////////////////
+// var SpotifyKey = SpotifyRef.on('value', function(snapshot){
+// 	console.log(snapshot.val().Key);
+// });
