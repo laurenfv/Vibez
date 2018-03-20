16a790e8ed71186c8cdd15096f579c68b8b9cfca
Lauren Ventura <you@example.com>
2018-03-20 19:45:57 -0400

    testing

--- assets/js/modules/spoitfyAjax.js
+++ assets/js/modules/spoitfyAjax.js
@@ -1,26 +1,26 @@
 
-var urlTracks = 'https://api.spotify.com/v1/tracks';
-var urlAudioFeatures = 'https://api.spotify.com/v1/audio-features?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B';
-var urlCategories = 'https://api.spotify.com/v1/browse/categories/mood/playlists?country=US&limit=50'
+// var urlTracks = 'https://api.spotify.com/v1/tracks';
+// var urlAudioFeatures = 'https://api.spotify.com/v1/audio-features?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B';
+// var urlCategories = 'https://api.spotify.com/v1/browse/categories/mood/playlists?country=US&limit=50'
 
-var categoriesArray = [];
+// var categoriesArray = [];
 
-$.ajax({
-   url: urlCategories,
-   type: "GET",
-   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-   success: function(data) { 
-     // Do something with the returned data
-      console.log(data);
-//       console.log(data.audio_features.energy);
-//       console.log(data.items.energy);
-//       console.log(data.items.audio_features.energy);
-        for (i = 0; data.categories.items.length; i++){
-            console.log(data.categories.items[i].id);
-        }
-     //       
-   }
-});
+// $.ajax({
+//    url: urlCategories,
+//    type: "GET",
+//    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+//    success: function(data) { 
+//      // Do something with the returned data
+//       console.log(data);
+// //       console.log(data.audio_features.energy);
+// //       console.log(data.items.energy);
+// //       console.log(data.items.audio_features.energy);
+//         for (i = 0; data.categories.items.length; i++){
+//             console.log(data.categories.items[i].id);
+//         }
+//      //       
+//    }
+// });
 
 
 // SCOPES's array 
--- assets/js/modules/spotifyConnect.js
+++ assets/js/modules/spotifyConnect.js
@@ -52,3 +52,26 @@ const scopes = [
 if (!_token) {
   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
 }
+
+var urlTracks = 'https://api.spotify.com/v1/tracks';
+var urlAudioFeatures = 'https://api.spotify.com/v1/audio-features?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B';
+var urlCategories = 'https://api.spotify.com/v1/browse/categories/mood/playlists?country=US&limit=50'
+
+var categoriesArray = [];
+
+$.ajax({
+   url: urlCategories,
+   type: "GET",
+   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+   success: function(data) { 
+     // Do something with the returned data
+      console.log(data);
+//       console.log(data.audio_features.energy);
+//       console.log(data.items.energy);
+//       console.log(data.items.audio_features.energy);
+        for (i = 0; data.categories.items.length; i++){
+            console.log(data.categories.items[i].id);
+        }
+     //       
+   }
+});
