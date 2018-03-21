83359a424df0c48b96659682bc194bc9af333d17
Lauren Ventura <you@example.com>
2018-03-20 23:20:51 -0400

    settled on URL parameters for api call

--- assets/js/modules/spotifyConnect.js
+++ assets/js/modules/spotifyConnect.js
@@ -53,10 +53,10 @@ if (!_token) {
   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
 }
 
-var urlTracks = 'https://api.spotify.com/v1/tracks';
-var urlAudioFeatures = 'https://api.spotify.com/v1/audio-features?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B';
-var urlPlaylists = 'https://api.spotify.com/v1/browse/categories/mood/playlists?country=US&limit=50'
-var urlCategories = 'https://api.spotify.com/v1/browse/categories?country=US&limit=50&offset=5'
+// var urlTracks = 'https://api.spotify.com/v1/tracks';
+// var urlAudioFeatures = 'https://api.spotify.com/v1/audio-features?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B';
+// var urlPlaylists = 'https://api.spotify.com/v1/browse/categories/mood/playlists?country=US&limit=50'
+// var urlCategories = 'https://api.spotify.com/v1/browse/categories?country=US&limit=50&offset=5'
 var urlRecommendations = 'https://api.spotify.com/v1/recommendations?seed_genres=pop,hip-hop,rock,latin,indie&min_valence=0.8&max_valence=1.0&min_energy=0.8&max_energy=1.0&min_dancibility=0.8&max_dancibility=1.0&limit=5&market=US'
 
 var categoriesArray = [];
