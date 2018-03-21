a80ff3bea43489fba7523801f94af728649d4fa4
VirtualVasquez <mxv647@med.miami.edu>
2018-03-20 21:30:50 -0400

    Retrying Firebase

--- assets/js/modules/firebase.js
+++ assets/js/modules/firebase.js
@@ -13,64 +13,75 @@ var config = {
 firebase.initializeApp(config);
 
 //variable to reference the database
-var databaseRef = firebase.database();
+var database = firebase.database();
 //variable to reference Azure
-var AzureRef = firebase.database().ref().child("Azure");
-var SpotifyRef = firebase.database().ref().child("Spotify");
-var SongsRef = firebase.database().ref().child("Songs");
-
+var AzureRef = database.ref().child("Azure");
+//variable to reference Spotify Song IDs
+var SongsRef = database.ref().child("Songs");
+//Variable to reference User String Inputs
+var StringsRef = database.ref("Strings");
+//used to reference testing HTML
 var show =  document.getElementById("test");
+
+console.log(AzureRef);
 
 //WRITE TO DATABASE FUNCTIONS
 ///////////////////////////////////////////////////////////////////////////////
 
+//API keys for Azure are stored on the database beforehand
+//Song IDs were added to Firebase via a JSON file
 //Test to see if JS is working
 console.log("okay");
-
-//API keys for Azure and Spotify are stored on the database beforehand
-//Song IDs were added to Firebase via a JSON file
-
-//Need to record user logins? At minimum, store unique ID to let people use app
-
-//==Also record previous data entries of the user
-
-
+//Capture User String
+var storeStrings = function(){
+	StringsRef.ref().on("value", function(snapshot){
+		StringsRef.push({
+			Text: //"VAR = VALUE OF USER INPUT HERE!"//
+		})
+	});
+};
 //READ FROM DATABASE FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////
-
-//Need to read/pull array of the songs from Firebase
-var songIDs = SongsRef.on('value', function(snapshot){
-	console.log(Object.values(snapshot.val()));
-	// console.log(snapshot.val().song01);
-	// console.log(snapshot.val().song02);
-	// console.log(snapshot.val().song03);
-	// console.log(snapshot.val().song04);
-	// //need to loop from song00 to song99, how?
-	// console.log(snapshot.val().song05);
-	// console.log(snapshot.val().song06);
-	// console.log(snapshot.val().song07);
-	// console.log(snapshot.val().song08);
-	// console.log(snapshot.val().song09);
-	// console.log(snapshot.val().song10);
-	// console.log(snapshot.val().song11);
-
-});
+//Need to pull array of the song IDs from Firebase
+var getSongIDs = function(){
+	SongsRef.on('value', function(snapshot){
+	window.songsArray = Object.values(snapshot.val());
+	});
+};
+//Retrieve User Strings as an array
+ var getStrings = function(){
+ 	StringsRef.on('value', function(snapshot){
+	window.stringsArray = Object.values(snapshot.val());
+ 	};
+};
 //Azure API Key
 ///////////////////////////////////////////////////////
-//STILL NEED CLARIFICATION ON HOW TO USE THE FUNCTIONS
-///////////////////////////////////////////////////////
-var AzureKey1 = AzureRef.once('value', function(snapshot){
-	window.azureKey1 = snapshot.val().Key1;
-	console.log(window.azureKey1);
-});
-////IF we need it, here is the 2nd key
-// var AzureKey2 = AzureRef.on('value', function(snapshot){
-// 	console.log(snapshot.val().Key2);
+// var AzureKey1 = AzureRef.once('value', function(snapshot){
+// 	window.azureKey1 = snapshot.val().Key1;
+// 	console.log(window.azureKey1);
 // });
-// ////////////////////////////////////////////////////////
+// var getStuffFromAzure = function(apiKey){
+// 	   $.ajax({
+//         url: "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + userInput,
+//         beforeSend: function(xhrObj){
+//             // Request headers
+//             xhrObj.setRequestHeader("Content-Type","application/json");
+//             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+//         },
+//         type: "POST",
+//         // Request body
+//         data: JSON.stringify(params),
+//     }).then(function(data){console.log(database)})
+// // }
 
-// //Spotify - API Key not yet defined, but this calls it
-// ////////////////////////////////////////////////////////
-// var SpotifyKey = SpotifyRef.on('value', function(snapshot){
-// 	console.log(snapshot.val().Key);
-// });
+var getDBValByPath = function(path, callback){
+	database.ref(path).once("value").then( function(snap){callback(snap.val())});
+};
+
+// console.log(getDBValByPath);
+
+// getDBValByPath("Azure/Key1", getStuffFromAzure);
+
+///////////////////////////////////////////////////////////
+
+//AzureKey1
