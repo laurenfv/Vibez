9a9f0d2fde053b3e3d9637258a3962be577eb93e
VirtualVasquez <mxv647@med.miami.edu>
2018-03-21 20:16:56 -0400

    Finally have things storing in Firebase and populating to test.html. Will move on to DOM JS and event listeners

--- assets/js/modules/firebase.js
+++ assets/js/modules/firebase.js
@@ -23,65 +23,57 @@ var StringsRef = database.ref("Strings");
 //used to reference testing HTML
 var show =  document.getElementById("test");
 
-console.log(AzureRef);
-
 //WRITE TO DATABASE FUNCTIONS
 ///////////////////////////////////////////////////////////////////////////////
-
 //API keys for Azure are stored on the database beforehand
 //Song IDs were added to Firebase via a JSON file
 //Test to see if JS is working
 console.log("okay");
-//Capture User String
-var storeStrings = function(){
-	StringsRef.ref().on("value", function(snapshot){
+
+//firebase setter
+var sendString = function(string){
 		StringsRef.push({
-			Text: //"VAR = VALUE OF USER INPUT HERE!"//
-		})
-	});
-};
-//READ FROM DATABASE FUNCTIONS
-////////////////////////////////////////////////////////////////////////////////
-//Need to pull array of the song IDs from Firebase
-var getSongIDs = function(){
-	SongsRef.on('value', function(snapshot){
-	window.songsArray = Object.values(snapshot.val());
-	});
-};
-//Retrieve User Strings as an array
- var getStrings = function(){
- 	StringsRef.on('value', function(snapshot){
-	window.stringsArray = Object.values(snapshot.val());
- 	};
-};
-//Azure API Key
-///////////////////////////////////////////////////////
-// var AzureKey1 = AzureRef.once('value', function(snapshot){
-// 	window.azureKey1 = snapshot.val().Key1;
-// 	console.log(window.azureKey1);
-// });
-// var getStuffFromAzure = function(apiKey){
-// 	   $.ajax({
-//         url: "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + userInput,
-//         beforeSend: function(xhrObj){
-//             // Request headers
-//             xhrObj.setRequestHeader("Content-Type","application/json");
-//             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
-//         },
-//         type: "POST",
-//         // Request body
-//         data: JSON.stringify(params),
-//     }).then(function(data){console.log(database)})
-// // }
-
-var getDBValByPath = function(path, callback){
-	database.ref(path).once("value").then( function(snap){callback(snap.val())});
+			search: string
+    }
+		);
 };
 
-// console.log(getDBValByPath);
+//form utility
+var resetForm = function(form){
+  $("input").val("");
+};
+var getFieldValue = function(form){
+  return $("#form").find("input").val();
+};
 
-// getDBValByPath("Azure/Key1", getStuffFromAzure);
+//event handler
+var handleStringSubmit = function(event){
+  sendString(getFieldValue(this));
+  resetForm(this);
+};
 
-///////////////////////////////////////////////////////////
+var handleStringPush = function(snapshot){
+  updateStrings(snapshot);
+}
 
-//AzureKey1
+//UI change
+var updateStrings = function(snapshot){
+  if (snapshot.val().search === 0){
+    return;
+  }
+  //*READS FROM FIREBASE
+  $("#stringHolder").prepend("<p>" + snapshot.val().search + "</p>")
+  //*READS FROM FIREBASE
+}
+
+//Bind Event Handler
+$("#form").submit(handleStringSubmit);
+
+
+StringsRef.on("child_added", handleStringPush);
+  //Whenever a child is added to StringsRef, the function handleStringPush is activated
+    //handleStringPush is the event handler, "getFieldValue" will capture the user's
+    //input upon clicking submit, and "sendString" will send it to Firebase
+
+
+//firebase getter
