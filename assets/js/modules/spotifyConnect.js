e906cdd3f2dfb3a3f3f009f8d80f798a019800a6
Gretel Martinez <gretelmn@gmail.com>
2018-03-22 21:36:53 -0400

    input content cleared

--- assets/js/modules/spotifyConnect.js
+++ assets/js/modules/spotifyConnect.js
@@ -200,6 +200,7 @@ function ajaxCall() {
              }
              console.log(displaySongs);
              appendIframes();
+             displaySongs = [];
           //       
         }
      })
