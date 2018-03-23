da85b2371541abbc5d8e6b3dacb2937c65bce9cc
Gretel Martinez <gretelmn@gmail.com>
2018-03-23 16:20:12 -0400

    user avatar

--- assets/js/modules/spotifyConnect.js
+++ assets/js/modules/spotifyConnect.js
@@ -72,7 +72,7 @@ if (!_token) {
            var userName = data.display_name;    
            var userImagesArray = data.images;    
            console.log(data.display_name);
-           $("'<span>Welcome, </span><span id="userNameSpan">'+userName+'</span>'").insertAfter( "#insertAfterHere" );
+           $('<span>Welcome, </span><span id="userNameSpan">'+userName+'</span>').insertAfter( "#insertAfterHere" );
           // Check if the user have profile image
            if (userImagesArray != emptyArray){
                $('#userAvatar').attr('src',userImagesArray[0]);
